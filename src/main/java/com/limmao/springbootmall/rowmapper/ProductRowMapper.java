a4fd2284bc503a22d674234fe51d1be4f172351e
limslmao <97270285+limslmao@users.noreply.github.com>
2024-01-19 17:37:13 +0800

    enum  and time

--- src/main/java/com/limmao/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/limmao/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.limmao.springbootmall.rowmapper;
 
+import com.limmao.springbootmall.constant.ProductCategory;
 import com.limmao.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,14 +15,18 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
         product.setDescription(resultSet.getString("description"));
         product.setCreatedDate(resultSet.getTimestamp("created_date"));
         product.setLastModifiedDate(resultSet.getTimestamp("last_modified_date"));
-        
+
         return product;
     }
 }
