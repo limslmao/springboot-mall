e3a567015b53ee62657bb59a4581b3bf7522e261
limslmao <97270285+limslmao@users.noreply.github.com>
2024-01-23 15:14:17 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- src/main/java/com/limmao/springbootmall/dao/ProductDao.java
+++ src/main/java/com/limmao/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.limmao.springbootmall.dao;
 import com.limmao.springbootmall.dto.ProductRequest;
 import com.limmao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/limmao/springbootmall/service/ProductService.java
+++ src/main/java/com/limmao/springbootmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.limmao.springbootmall.service;
 import com.limmao.springbootmall.dto.ProductRequest;
 import com.limmao.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
