911ec31863b20cf122ed5cc65ec17296805ebc66
limslmao <97270285+limslmao@users.noreply.github.com>
2024-01-26 11:36:05 +0800

    帳號功能 - 登入

--- src/main/java/com/limmao/springbootmall/controller/UserController.java
+++ src/main/java/com/limmao/springbootmall/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.limmao.springbootmall.controller;
 
+import com.limmao.springbootmall.dto.UserLoginRequest;
 import com.limmao.springbootmall.dto.UserRegisterRequest;
 import com.limmao.springbootmall.model.User;
 import com.limmao.springbootmall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +20,18 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/users/register")
-    public ResponseEntity<User> register(@RequestBody @Valid UserRegisterRequest userRegisterRequest){
+    public ResponseEntity<User> register(@RequestBody @Valid UserRegisterRequest userRegisterRequest) {
         Integer userId = userService.register(userRegisterRequest);
 
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/limmao/springbootmall/service/UserService.java
+++ src/main/java/com/limmao/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.limmao.springbootmall.service;
 
+import com.limmao.springbootmall.dto.UserLoginRequest;
 import com.limmao.springbootmall.dto.UserRegisterRequest;
 import com.limmao.springbootmall.model.User;
 
@@ -8,4 +9,6 @@ public interface UserService {
     User getUserById(Integer userId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
